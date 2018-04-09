35f75815b371eb3b9af3dc6825ae7b4960902b4d
Zach Godin <sayhello@zachgod.in>
2018-04-09 15:45:07 -0400

    Added click-to-scroll snippet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 
+    //Scroll to element
+    $(".scrollTo").click(function(e) {
+      e.preventDefault();
+      var href = $(this).attr("href");
+      $('html, body').animate({scrollTop:$(href).offset().top - 50}, 1200);    
+    });
+  
 });
