aae4cb11edd900c15d24579601fd75f70b467701
Zach Godin <sayhello@zachgod.in>
2018-09-05 17:50:06 +0100

    Update default.php
    General tidy up

--- default.php
+++ default.php
@@ -1,37 +1,12 @@
-<?php
-defined('C5_EXECUTE') or die(_("Access Denied."));
-$this->inc('elements/header.php');
-$tp = $this->getThemePath();
-function getTheURL($url_id) {
-	$opg = Page::getById($url_id);
-	$url=Loader::helper('navigation'); 
-	$canonical=$url->getCollectionURL($opg); 
-	$canonical=preg_replace("/index.php\?cID=1$/","",$canonical); 
-	echo $canonical;
-}
-function spaceToDash($string) {
-    $string = strtolower($string);
-    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-    $string = preg_replace("/[\s-]+/", " ", $string);
-    $string = preg_replace("/[\s_]/", "-", $string);
-    return $string;
-}
-$page = Page::getCurrentPage();
-$page_title = spaceToDash(strtolower($page->getCollectionName()));
+<?php defined('C5_EXECUTE') or die(_("Access Denied.")); $this->inc('elements/header.php'); $tp = $this->getThemePath(); function spaceToDash($string) { $string = strtolower($string); $string = preg_replace("/[^a-z0-9_\s-]/", "", $string); $string = preg_replace("/[\s-]+/", " ", $string); $string = preg_replace("/[\s_]/", "-", $string); return $string; } $page = Page::getCurrentPage();
+$page_title = spaceToDash(strtolower($page->getCollectionName())); ?>
 
-?>
-
-<div class="page-title container-fluid">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-            	<h1><?=$page->getCollectionName()?></h1>
-            </div>
-		</div>
-	</div>
+<div class="page-title">
+  <h1><?=$page->getCollectionName()?></h1>
+  <?php $m = new GlobalArea('Page Title'); $m->display($c); ?>
 </div>
 
-	<div class="content whiteback <?=$page_title?>">
+	<div class="pg-content pg-<?=$page_title?>">
 		<?php
 			$m = new Area('Content');
 			$m->enableGridContainer();
@@ -39,13 +14,4 @@ $page_title = spaceToDash(strtolower($page->getCollectionName()));
 		?>
 	</div>
 
-<!-- Example of a global content area -->
-	<?php 
-        $m = new GlobalArea('Content');
-        $m->display($c);
-    ?>
-
-
-<?php
-$this->inc('elements/footer.php');
-?>
+<?php $this->inc('elements/footer.php'); ?>
